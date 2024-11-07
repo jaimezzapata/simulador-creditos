533d897ceeaca8c33db6491773eaf0968525a8e2
jaimezzapata <zapataval2304@gmail.com>
2024-11-07 13:18:56 -0500

    Ajustes

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -33,33 +33,32 @@
 //     }
 // }
 
-document.querySelector('#btnFormRegistro').addEventListener('click', function () {
-    document.querySelector('#form-login').style.display = 'flex'
-    document.querySelector('#form-register').style.display = 'none'
-})
-document.querySelector('#btnFormLogin').addEventListener('click', function () {
-    document.querySelector('#form-login').style.display = 'none'
-    document.querySelector('#form-register').style.display = 'flex'
-})
-let inputs = document.getElementsByTagName('input')
+document
+  .querySelector("#btnFormRegistro")
+  .addEventListener("click", function () {
+    document.querySelector("#form-login").style.display = "flex";
+    document.querySelector("#form-register").style.display = "none";
+  });
+document.querySelector("#btnFormLogin").addEventListener("click", function () {
+  document.querySelector("#form-login").style.display = "none";
+  document.querySelector("#form-register").style.display = "flex";
+});
+let inputs = document.getElementsByTagName("input");
 for (let index = 0; index < inputs.length; index++) {
-    inputs[index].addEventListener('keyup', validarFormulario)
+  inputs[index].addEventListener("keyup", validarFormulario);
 }
 function validarFormulario(e) {
-    switch (e.target.name) {
-        case 'login-usuario':
-            console.log('Este es el input de login usuario')
-            break;
+  switch (e.target.name) {
+    case "login-usuario":
+      console.log("Este es el input de login usuario");
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
 
 // console.log(document.querySelectorAll('input'))
-
-
-
 
 // let arreglo = [1, 'jaime', true, function () { }, { nombre: 'jaime' }, []]
 // console.log(arreglo)
