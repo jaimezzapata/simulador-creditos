3f2968eab8504853b2d59bdb4cbb1081061eff3b
jaimezzapata <zapataval2304@gmail.com>
2024-11-12 14:42:57 -0500

    Se finaliza validación con expresiones regulares

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -22,14 +22,75 @@ function validarFormulario(e) {
     case "register-nombre":
       if (expresionesRegulares.nombre.test(e.target.value)) {
         document.getElementById("register-nombre").classList.add("correcto");
-        document.getElementById("register-nombre").classList.remove("incorrecto");
+        document
+          .getElementById("register-nombre")
+          .classList.remove("incorrecto");
+        // document.getElementById("mensaje-nombre").textContent =
+        // "";
       } else {
         document.getElementById("register-nombre").classList.add("incorrecto");
         document.getElementById("register-nombre").classList.remove("correcto");
+        // document.getElementById("mensaje-nombre").textContent =
+        //   "El nombre solo puede ir en mayúsculas";
       }
       break;
-
-    default:
+    case "register-usuario":
+      if (expresionesRegulares.usuario.test(e.target.value)) {
+        document.getElementById("register-usuario").classList.add("correcto");
+        document
+          .getElementById("register-usuario")
+          .classList.remove("incorrecto");
+        document.getElementById("mensaje-nombre").textContent =
+          "El nombre solo permite mayúsculas";
+      } else {
+        document.getElementById("register-usuario").classList.add("incorrecto");
+        document
+          .getElementById("register-usuario")
+          .classList.remove("correcto");
+      }
+      break;
+    case "register-correo":
+      if (expresionesRegulares.correo.test(e.target.value)) {
+        document.getElementById("register-correo").classList.add("correcto");
+        document
+          .getElementById("register-correo")
+          .classList.remove("incorrecto");
+      } else {
+        document.getElementById("register-correo").classList.add("incorrecto");
+        document.getElementById("register-correo").classList.remove("correcto");
+      }
+      break;
+    case "register-contrasena":
+      if (expresionesRegulares.contraseña.test(e.target.value)) {
+        document
+          .getElementById("register-contrasena")
+          .classList.add("correcto");
+        document
+          .getElementById("register-contrasena")
+          .classList.remove("incorrecto");
+      } else {
+        document
+          .getElementById("register-contrasena")
+          .classList.add("incorrecto");
+        document
+          .getElementById("register-contrasena")
+          .classList.remove("correcto");
+      }
+      break;
+    case "register-confirmar":
+      if (expresionesRegulares.contraseña.test(e.target.value)) {
+        document.getElementById("register-confirmar").classList.add("correcto");
+        document
+          .getElementById("register-confirmar")
+          .classList.remove("incorrecto");
+      } else {
+        document
+          .getElementById("register-confirmar")
+          .classList.add("incorrecto");
+        document
+          .getElementById("register-confirmar")
+          .classList.remove("correcto");
+      }
       break;
   }
 }
